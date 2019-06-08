e73460ac1f4745dc5096527a1abb13f40f022d2a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-08 11:53:18 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/fragment2802/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragment2802/MainActivity.java
@@ -1,9 +1,14 @@
 package khoapham.ptp.phamtanphat.fragment2802;
 
+import android.os.CountDownTimer;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
 //            + Fragment xml : nó chỉ nhìn vào kích thước view con để hiển thị
 //                android:layout_width="wrap_content"
 //                android:layout_height="wrap_content"
+//            + Khi đưa fragment vào xml phải có id và name
+        //2 : Sư dụng fragment cho code
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.add(R.id.framelayout,androidFragment);
+        fragmentTransaction.commit();
+
     }
 }
